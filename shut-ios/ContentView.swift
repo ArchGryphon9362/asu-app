d04fbafbfd2032a8337f6980dd4cc3a9e881b203
ArchGryphon9362 <156611467+ArchGryphon9362@users.noreply.github.com>
2023-09-25 12:22:15 +0100

    Initial commit

---
+++ shut-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shut-ios
+//
+//  Created by ArchGryphon9362 on 25/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shut-ios/shut_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shut_iosApp.swift
+//  shut-ios
+//
+//  Created by ArchGryphon9362 on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct shut_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
