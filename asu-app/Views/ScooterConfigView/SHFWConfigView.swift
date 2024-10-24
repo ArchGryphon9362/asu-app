e6c79efffdb622969b02483df3a0bcefbf0faa83
ArchGryphon9362 <156611467+ArchGryphon9362@users.noreply.github.com>
2024-10-24 23:40:23 +0100

    improve config layout

--- asu-app/Views/ScooterConfigView/SHFWConfigView.swift
+++ asu-app/Views/ScooterConfigView/SHFWConfigView.swift
@@ -7,18 +7,34 @@
 
 import SwiftUI
 
+private struct ProfileOptionsView: View {
+    @Binding var selectedProfile: Int
+    
+    var body: some View {
+        Section(header: Text("Profile")) {
+            VStack(alignment: .leading) {
+                Text("Selected Profile")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+                Picker("", selection: self.$selectedProfile) {
+                    Text("1").tag(0)
+                    Text("2").tag(1)
+                    Text("3").tag(2)
+                }.pickerStyle(.segmented)
+            }
+        }
+    }
+}
+
 private struct ProfileConfigView: View {
     @ObservedObject var profile: ScooterManager.SHFWProfile
     
     var body: some View {
-        Section {
-            // text field examples
-            NumericTF(name: "P3 sports 1", value: $profile.sportsAmps[0], in: 0...100, step: 0.01)
-            NumericTF(name: "P3 sports 2", value: $profile.sportsAmps[1], in: 0...100, step: 0.01)
-            // slider examples
-            ReleaseSlider(name: "P3 sports 3", value: $profile.sportsAmps[2], in: 0...100, step: 0.01)
-            ReleaseSlider(name: "P3 sports 4", value: $profile.sportsAmps[3], in: 0...100, step: 0.01)
-            
+        Section(header: Text("Throttle")) {
+            ReleaseSlider(name: "Sports 1", value: self.$profile.sportsAmps[0], in: 0...100, step: 0.01)
+            ReleaseSlider(name: "Sports 2", value: self.$profile.sportsAmps[1], in: 0...100, step: 0.01)
+            ReleaseSlider(name: "Sports 3", value: self.$profile.sportsAmps[2], in: 0...100, step: 0.01)
+            ReleaseSlider(name: "Sports 4", value: self.$profile.sportsAmps[3], in: 0...100, step: 0.01)
         }
     }
 }
@@ -27,9 +43,9 @@ private struct SystemConfigView: View {
     @ObservedObject var global: ScooterManager.SHFWGlobal
     
     var body: some View {
-        Section {
+        Section(header: Text("System Settings")) {
             // pwm
-            ReleaseSlider(name: "PWM", value: $global.pwm, in: 4...24, step: 4)
+            ReleaseSlider(name: "PWM", value: self.$global.pwm, in: 4...24, step: 4)
         }
     }
 }
@@ -37,12 +53,13 @@ private struct SystemConfigView: View {
 struct SHFWConfigView: View {
     @ObservedObject var shfw: ScooterManager.SHFW
 
-    @State var selectedProfile: Int = 2
+    @State var selectedProfile: Int = 0
     
     var body: some View {
         VStack {
             if let config = self.shfw.config {
                 List {
+                    ProfileOptionsView(selectedProfile: self.$selectedProfile)
                     ProfileConfigView(profile: config.getProfile(self.selectedProfile))
                     SystemConfigView(global: config.global)
                 }
